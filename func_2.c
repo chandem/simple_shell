0b363327dd7c59c3bb97a06b3c8602965a587301
chandem <49794721+chandem@users.noreply.github.com>
2022-09-07 03:24:03 +0300

    Update func_2.c

--- func_2.c
+++ func_2.c
@@ -1,13 +1,48 @@
-#include <unistd.h>
-
+#include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * execute_proc - similar to puts in C
+ * @cmd: a pointer the integer we want to set to 402
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: int
  */
-int _putchar(char c)
+void execute_proc(char **cmd)
 {
-	return (write(1, &c, 1));
+
+	char *parametro = (*(cmd + 1));
+	char *s, *slash = "/";
+	char *o;
+
+	char *vartoprint = *cmd;
+	char *argv[4];
+
+	if ((access(cmd[0], F_OK) == 0))
+	{
+		argv[0] = cmd[0];
+		argv[1] = parametro;
+		argv[2] = ".";
+		argv[3] = NULL;
+
+		if (execve(argv[0], argv, NULL) == -1)
+		{
+			perror("Error");
+		}
+	}
+	else
+	{
+		o = find_command(vartoprint);
+
+		slash = str_concat(o, slash);
+
+		s = str_concat(slash, *cmd);
+
+		argv[0] = s;
+		argv[1] = parametro;
+		argv[2] = ".";
+		argv[3] = NULL;
+
+		if (execve(argv[0], argv, NULL) == -1)
+		{
+			perror("Error");
+		}
+	}
 }
